38341fe412afd408a638c30c319319334e42deea
Ezra Gonyuie <ezragonyuie@gmail.com>
2024-09-12 17:20:17 +0200

    Works on localhost

--- src/app/api/youtube/upload/route.ts
+++ src/app/api/youtube/upload/route.ts
@@ -4,20 +4,6 @@ import fs from "fs";
 import path from "path";
 import { oauth2Client } from "@/utils/oauthClient";
 
-// type MulterError = multer.MulterError | null;
-
-// // Configure multer with disk storage
-// const upload = multer({
-//   storage: multer.diskStorage({
-//     destination: (req, file, cb) => {
-//       cb(null, path.join(process.cwd(), "public", "videos")); // Point to public/videos
-//     },
-//     filename: (req, file, cb) => {
-//       cb(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
-//     },
-//   }),
-// });
-
 // Initialize the YouTube API client
 const youtube = google.youtube({
   version: "v3",
@@ -25,17 +11,7 @@ const youtube = google.youtube({
 });
 
 export async function POST(request: NextRequest) {
-  //   const { code } = await request.json();
-  console.log("Token from cookies:", request.cookies.get("token")?.value);
-  console.log(
-    "Refresh token from cookies:",
-    request.cookies.get("refreshToken")?.value
-  );
-
   try {
-    // const { tokens } = await oauth2Client.getToken(code);
-    // oauth2Client.setCredentials(tokens);
-
     // Get form data
     const formData = await request.formData();
     const title = formData.get("title")?.toString();
@@ -63,6 +39,9 @@ export async function POST(request: NextRequest) {
     // Ensure tokens are set on the oauth2Client
     const cookies = request.cookies.get("token")?.value;
     const refreshToken = request.cookies.get("refreshToken")?.value;
+
+    console.log("Token from cookies:", cookies);
+    console.log("Refresh token from cookies:", refreshToken);
 
     if (cookies && refreshToken) {
       oauth2Client.setCredentials({
