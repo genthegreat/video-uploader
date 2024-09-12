71502e70b403e9b0fab8327dc2501a8caa1c2a42
Ezra Gonyuie <ezragonyuie@gmail.com>
2024-09-13 01:28:30 +0200

    Smooth transition between pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -16,10 +17,9 @@ export default function Home() {
         <h1 className="w-full text-4xl text-center">Video Uploader</h1>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
+          <Link
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="/api/youtube/auth"
-            rel="noopener noreferrer"
+            href="/upload"
           >
             <Image
               className="dark:invert"
@@ -29,14 +29,14 @@ export default function Home() {
               height={20}
             />
             Go to Uploader
-          </a>
-          <a
+          </Link>
+          <Link
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="#"
             rel="noopener noreferrer"
           >
             Read our docs
-          </a>
+          </Link>
         </div>
       </main>      
     </div>
