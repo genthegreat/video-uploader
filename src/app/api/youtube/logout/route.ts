05f9bb60185b56e1f52648546d371587c6865f82
Ezra Gonyuie <ezragonyuie@gmail.com>
2024-09-13 03:33:13 +0200

    corrected logout route

--- src/app/api/youtube/logout/route.ts
+++ src/app/api/youtube/logout/route.ts
@@ -1,8 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) { 
-    request.cookies.delete('token')
-    request.cookies.delete('refreshToken')
-
-    return NextResponse.redirect(new URL('/', request.url).toString());
+    // Create a new response object
+    const response = NextResponse.redirect(new URL('/', request.url).toString());
+    
+    // Set cookies with expired date to delete them
+    response.cookies.delete('token');
+    response.cookies.delete('refreshToken');
+    
+    return response;
 }
